159b2ac680abd0dec1ac3a3cc6c70b99a8f72564
omaromarsalhi <omar.salhi.job@gmail.com>
2024-08-31 23:17:06 +0100

    start working on the authentication system

--- moquawel/authentification/src/main/resources/db/migration/V3__init_database.sql
+++
@@ -1,9 +0,0 @@
--- CREATE TABLE token_blacklist
--- (
---     idToken         integer PRIMARY KEY,
---     jti        VARCHAR(255) NOT NULL,
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---     expires_at TIMESTAMP
--- );
---
--- CREATE SEQUENCE token_blacklist_SEQ START WITH 1 INCREMENT BY 1;
--- moquawel/authentification/src/main/resources/db/migration/V4__init_database.sql
+++
@@ -1,3 +0,0 @@
---
--- alter table token_blacklist add COLUMN idUser integer;
--- alter table token_blacklist add constraint fk_tokens foreign key (idUser) references users;
