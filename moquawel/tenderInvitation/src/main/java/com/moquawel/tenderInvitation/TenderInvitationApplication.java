ba15262d66cab89881f8fc5b6f1bc71020c14599
omaromarsalhi <omar.salhi.job@gmail.com>
2024-09-19 22:30:28 +0100

    start working on the tender invitation

---
+++ moquawel/tenderInvitation/src/main/java/com/moquawel/tenderInvitation/TenderInvitationApplication.java
@@ -0,0 +1,13 @@
+package com.moquawel.tenderInvitation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenderInvitationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenderInvitationApplication.class, args);
+	}
+
+}
---
+++ moquawel/tenderInvitation/src/test/java/com/moquawel/tenderInvitation/TenderInvitationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moquawel.tenderInvitation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenderInvitationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
