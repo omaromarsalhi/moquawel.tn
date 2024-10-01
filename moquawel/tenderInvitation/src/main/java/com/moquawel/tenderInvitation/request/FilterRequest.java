e22fed821eed2363793ddd14fe280837adae714c
omaromarsalhi <omar.salhi.job@gmail.com>
2024-10-01 11:21:38 +0100

    adding validation to the user input

--- moquawel/tenderInvitation/src/main/java/com/moquawel/tenderInvitation/request/FilterRequest.java
+++ moquawel/tenderInvitation/src/main/java/com/moquawel/tenderInvitation/request/FilterRequest.java
@@ -2,10 +2,23 @@ package com.moquawel.tenderInvitation.request;
 
 
 import java.time.LocalDateTime;
+import jakarta.validation.constraints.*;
+
 
 public record FilterRequest(
+        @NotNull(message = "Public date must not be null")
+        @PastOrPresent(message = "Public date must be in the past or present")
         LocalDateTime publicDt,
+
+        @NotNull(message = "BD Receive End date must not be null")
+        @Future(message = "BD Receive End date must be in the future")
         LocalDateTime bdRecvEndDt,
+
+        @NotEmpty(message = "Category must not be empty")
+        @NotNull(message = "Category must not be null")
+        @Size(min = 3, max = 50, message = "Category must be between 3 and 50 characters")
+        @Pattern(regexp = "^[A-Za-z]+$", message = "Category must only contain alphabetic characters")
         String category
 ) {
 }
+
